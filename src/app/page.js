112e849e2990581ffbd22ffab6313041fca3c373
sreerammelpadi <melpadisreeram@gmail.com>
2024-10-11 17:31:14 -0400

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <div>
+    <div>
+    Hello, my name is Sreeram
+    </div>
+    <div>
+    This is my first NextJS App
+    </div>
+  </div>
+}
